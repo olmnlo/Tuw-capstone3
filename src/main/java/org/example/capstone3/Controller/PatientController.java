8e6877458fe1ee5ee6392df1cb1252c5ba0d3507
Hussam Alghamdi <65325249+olmnlo@users.noreply.github.com>
2025-08-18 20:32:24 +0300

    get patients (#15)

--- src/main/java/org/example/capstone3/Controller/PatientController.java
+++ src/main/java/org/example/capstone3/Controller/PatientController.java
@@ -1,12 +1,26 @@
 package org.example.capstone3.Controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.capstone3.Model.Patient;
+import org.example.capstone3.Service.PatientService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping("/api/v1/patient")
 @RequiredArgsConstructor
 public class PatientController {
+
+    private final PatientService patientService;
+
+    @GetMapping
+    public ResponseEntity<List<Patient>> getAllPatients(){
+        return ResponseEntity.status(HttpStatus.OK).body(patientService.findAllPatient());
+    }
 
 }
