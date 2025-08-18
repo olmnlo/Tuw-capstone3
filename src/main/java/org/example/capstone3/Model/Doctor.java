a673a46547b01391ab2c4efc3c84df3f5491296e
Hussam Alghamdi <65325249+olmnlo@users.noreply.github.com>
2025-08-18 17:56:47 +0300

    done doctor and paitent (#8)

--- src/main/java/org/example/capstone3/Model/Doctor.java
+++ src/main/java/org/example/capstone3/Model/Doctor.java
@@ -1,6 +1,39 @@
 package org.example.capstone3.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Data
+//Hussam
 public class Doctor {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String specialization;
+
+    private String name;
+    private Integer age;
+    private Character sex;
+    private String bio;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "doctor")
+    private Set<Schedule> schedule;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Profile profile;
 
 }
