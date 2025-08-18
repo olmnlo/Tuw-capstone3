3f38e49dee7320894d07d73639a86bcb92e49097
Hussam Alghamdi <65325249+olmnlo@users.noreply.github.com>
2025-08-18 22:45:05 +0300

    the work of video is done (#25)

---
+++ src/main/java/org/example/capstone3/Repository/VideoRepository.java
@@ -0,0 +1,8 @@
+package org.example.capstone3.Repository;
+
+import org.example.capstone3.Model.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VideoRepository extends JpaRepository<VideoRepository, Integer> {
+    Video findVideoById(Integer id);
+}
