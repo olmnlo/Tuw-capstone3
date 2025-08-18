7db4799f561223ffc8bc37e3630a6d4e9439a07e
olmnlo <hosamag54@gmail.com>
2025-08-18 21:02:11 +0300

    add patient

--- src/main/java/org/example/capstone3/Model/Patient.java
+++ src/main/java/org/example/capstone3/Model/Patient.java
@@ -22,31 +22,20 @@ public class Patient {
     private Integer id;
 
 
-    @NotEmpty(message = "name is required")
-    @Size(max = 30, message = "name max length 30")
     @Column(columnDefinition = "varchar(30) not null")
     private String name;
 
-    @NotEmpty(message = "username is required")
-    @Size(max = 20, message = "username max length 20")
     @Column(columnDefinition = "varchar(20) unique not null")
     private String username;
 
-    @NotEmpty(message = "password is required")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?`~])(?=.{8,}).*$", message = "password must has 1 lower/upper 1 digit 1 special character")
-    @Size(max = 20, message = "password max length 20")
     @Column(columnDefinition = "varchar(20) not null")
     private String password;
 
-    @NotNull(message = "age is required")
-    @Positive(message = "age must be positive")
-    @Min(value = 16, message = "your age must be more than 16")
+    @Column(columnDefinition = "int not null")
     private Integer age;
 
 
-    @NotEmpty(message = "sex is required")
-    @Size(max = 6, message = "sex max length 6")
-    @Pattern(regexp = "^(male|female)$", message = "sex must be either male or female")
+    @Column(columnDefinition = "varchar(6) not null")
     private String sex;
 
 
