e96f92065257894c4d7154649352b7644bc51512
Aziz-Alharbi <ayh92000@gmail.com>
2025-08-18 18:27:38 +0300

    adding validations

--- src/main/java/org/example/capstone3/Model/Report.java
+++ src/main/java/org/example/capstone3/Model/Report.java
@@ -1,4 +1,20 @@
 package org.example.capstone3.Model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Report {
+
+    private Integer id;
+
+    private String description;
+
+    private Doctor doctor;
+
+    private Patient patient;
 }
--- src/main/java/org/example/capstone3/Model/Schedule.java
+++ src/main/java/org/example/capstone3/Model/Schedule.java
@@ -3,10 +3,20 @@ package org.example.capstone3.Model;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Schedule {
 
-    @Id
     private Integer id;
+
+    private LocalDate date;
     @ManyToOne
     private Doctor doctor;
 }
