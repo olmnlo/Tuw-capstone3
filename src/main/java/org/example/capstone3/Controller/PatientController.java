5aaf84c1db6e6350c8fe0e8b058d97a14c677e14
Aziz-Alharbi <150168865+Aziz-Alharbi@users.noreply.github.com>
2025-08-18 21:10:24 +0300

    Azoz (#20)
    * finished the doctor layers
    
    * Updating azoz (#17)
    
    * get patients (#15)
    
    * finished the doctor layers (#16)
    
    ---------
    
    Co-authored-by: Hussam Alghamdi <65325249+olmnlo@users.noreply.github.com>
    
    * patient (#19)
    
    * get patients (#15)
    
    * finished the doctor layers (#16)
    
    ---------
    
    Co-authored-by: Hussam Alghamdi <65325249+olmnlo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Hussam Alghamdi <65325249+olmnlo@users.noreply.github.com>

--- src/main/java/org/example/capstone3/Controller/PatientController.java
+++ src/main/java/org/example/capstone3/Controller/PatientController.java
@@ -8,7 +8,13 @@ import org.example.capstone3.Model.Patient;
 import org.example.capstone3.Service.PatientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
 import java.util.List;
 
 @RestController
@@ -17,11 +23,13 @@ import java.util.List;
 public class PatientController {
 
     private final PatientService patientService;
+
     //Hussam
     @GetMapping
     public ResponseEntity<List<Patient>> getAllPatients(){
         return ResponseEntity.status(HttpStatus.OK).body(patientService.findAllPatient());
     }
+
 
     //Hussam
     @PostMapping
