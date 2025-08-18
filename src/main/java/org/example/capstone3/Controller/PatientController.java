53e0013105d55e5c38e3f343c3b14bb7864b61a0
Hussam Alghamdi <65325249+olmnlo@users.noreply.github.com>
2025-08-18 21:04:53 +0300

    Task1 hussam (#18)
    * get patients
    
    * add patient

--- src/main/java/org/example/capstone3/Controller/PatientController.java
+++ src/main/java/org/example/capstone3/Controller/PatientController.java
@@ -1,14 +1,14 @@
 package org.example.capstone3.Controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.example.capstone3.Api.ApiResponse;
+import org.example.capstone3.DTO.PatientDTO;
 import org.example.capstone3.Model.Patient;
 import org.example.capstone3.Service.PatientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
@@ -17,10 +17,19 @@ import java.util.List;
 public class PatientController {
 
     private final PatientService patientService;
-
+    //Hussam
     @GetMapping
     public ResponseEntity<List<Patient>> getAllPatients(){
         return ResponseEntity.status(HttpStatus.OK).body(patientService.findAllPatient());
     }
+
+    //Hussam
+    @PostMapping
+    public ResponseEntity<ApiResponse> addNewPatient(@Valid@RequestBody PatientDTO patientDTO){
+        patientService.addPatient(patientDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("patient added successfully"));
+    }
+
+
 
 }
--- src/main/java/org/example/capstone3/Service/PatientService.java
+++ src/main/java/org/example/capstone3/Service/PatientService.java
@@ -2,6 +2,7 @@ package org.example.capstone3.Service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.capstone3.Api.ApiException;
+import org.example.capstone3.DTO.PatientDTO;
 import org.example.capstone3.Model.Patient;
 import org.example.capstone3.Repository.PatientRepository;
 import org.springframework.stereotype.Service;
@@ -20,5 +21,16 @@ public class PatientService {
             throw new ApiException("no patients");
         }
         return patients;
+    }
+
+    //Hussam
+    public void addPatient(PatientDTO patientDTO){
+        Patient patient = new Patient(null, patientDTO.getName(),patientDTO.getUsername(),patientDTO.getPassword(), patientDTO.getAge(), patientDTO.getSex(), null, null, null, null);
+        patientRepository.save(patient);
+    }
+
+    //Hussam
+    public void updatePatient(Integer patient_id, Patient patient){
+
     }
 }
