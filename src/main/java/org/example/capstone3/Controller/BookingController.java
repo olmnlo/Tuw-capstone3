f59be004fa8014faf3b0635dd4363776042bd148
MohammedShmhani <158126523+MohammedShmhani@users.noreply.github.com>
2025-08-19 13:42:28 +0300

    create end point in booking service and controller (#38)

--- src/main/java/org/example/capstone3/Controller/BookingController.java
+++ src/main/java/org/example/capstone3/Controller/BookingController.java
@@ -6,6 +6,7 @@ import org.example.capstone3.Model.Booking;
 import org.example.capstone3.Service.BookingService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 //Hussam: general fix
 //Mohammed
 @RestController
@@ -19,23 +20,35 @@ public class BookingController {
     public ResponseEntity<?> getAllBookings() {
         return ResponseEntity.ok().body(bookingService.getAllBookings());
     }
+
     //Hussam: fix make it small letters and clear
     @PostMapping("/patient/{patient_id}/doctor/{doctor_id}")
-    public ResponseEntity<?> createBooking(@PathVariable Integer patient_id,@PathVariable Integer doctor_id) {
+    public ResponseEntity<?> createBooking(@PathVariable Integer patient_id, @PathVariable Integer doctor_id) {
         bookingService.addBooking(patient_id, doctor_id);
         return ResponseEntity.ok().body(new ApiResponse("Booking has been created"));
     }
 
     //Hussam: fix make it small letters and clear
     @DeleteMapping("/patient/{patient_id}/booking/{booking_id}")
-    public ResponseEntity<?> deleteBooking(@PathVariable Integer patient_id,@PathVariable Integer booking_id) {
+    public ResponseEntity<?> deleteBooking(@PathVariable Integer patient_id, @PathVariable Integer booking_id) {
         bookingService.removeBooking(patient_id, booking_id);
         return ResponseEntity.ok().body(new ApiResponse("Booking has been deleted"));
     }
+
     //Hussam: fix make it small letters and clear
     @PutMapping("/doctor/{doctor_id}/booking{booking_id}/status")
-    public ResponseEntity<?> updateBooking(@PathVariable Integer doctor_id, @PathVariable Integer booking_id,@RequestParam("status") String status) {
+    public ResponseEntity<?> updateBooking(@PathVariable Integer doctor_id, @PathVariable Integer booking_id, @RequestParam("status") String status) {
         bookingService.updateBooking(doctor_id, booking_id, status);
         return ResponseEntity.ok().body(new ApiResponse("Booking has been updated"));
+    }
+
+    //Mohammed
+    @GetMapping("/patient/{patientId}/status/{status}/on/{on}/from/{from}/to/{to}")
+    public ResponseEntity<?> getPatientBookingsSimple(@PathVariable Integer patientId,
+                                                      @PathVariable String status,
+                                                      @PathVariable String on,   // yyyy-MM-dd (اختياري)
+                                                      @PathVariable String from, // yyyy-MM-dd (اختياري)
+                                                      @PathVariable String to) {
+        return ResponseEntity.ok().body(bookingService.getPatientBookingsSimple(patientId, status, on, from, to));
     }
 }
