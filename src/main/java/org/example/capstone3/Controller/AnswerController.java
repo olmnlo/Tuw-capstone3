6004e468864e67f750c3387722ef12c16b8ae52e
olmnlo <hosamag54@gmail.com>
2025-08-20 11:20:56 +0300

    35

--- src/main/java/org/example/capstone3/Controller/AnswerController.java
+++ src/main/java/org/example/capstone3/Controller/AnswerController.java
@@ -15,13 +15,14 @@ import org.springframework.web.bind.annotation.*;
 public class AnswerController {
     private final AnswerService answerService;
 
-
+    //ENDPOINT 1
     @PutMapping("assign/patient/{patient_id}")
     public ResponseEntity<ApiResponse> assignQuestionToPatient(@PathVariable Integer patient_id) {
         answerService.assignQuestionsToPatient(patient_id);
         return ResponseEntity.status(200).body(new ApiResponse("Successfully assigned questions to patient"));
     }
 
+    //ENDPOINT 2
     @PostMapping("/question/{question_id}/patient/{patient_id}/answer")
     public ResponseEntity<ApiResponse> answerQuestion(@PathVariable Integer patient_id,@PathVariable Integer question_id ,@Valid@RequestBody AnswerDTO answerDTO){
         answerService.answerTheQuestion(patient_id, question_id, answerDTO);
